d98d49d7d2915456d33300583ab6a311e6e417e9
nicholas3d2 <nicholas3d2@gmail.com>
2022-02-23 17:31:57 -0500

    Added get_accuracy and train function for baseline model.

--- baseline.py
+++ baseline.py
@@ -8,7 +8,7 @@ import torchvision
 from torch.utils.data.sampler import SubsetRandomSampler
 import torchvision.transforms as transforms
 
-torch.manual_seed(1)  # set the random seed
+torch.manual_seed(1000)  # set the random seed
 
 class Baseline(nn.Module):
     def __init__(self):
@@ -27,9 +27,78 @@ class Baseline(nn.Module):
         output = output.squeeze(1)
         return output
 
-#TRAIN BASELINE ...
+def get_accuracy(model, data):
+    correct = 0
+    total = 0
+    for imgs, labels in torch.utils.data.DataLoader(data, batch_size=64):
 
-#For viewing multiclass probabilities!
-prob = F.softmax(output, dim=1)
-print(prob)
-print(sum(prob[0]))
+        #############################################
+        # To Enable GPU Usage
+        if torch.cuda.is_available():
+          imgs = imgs.cuda()
+          labels = labels.cuda()
+        #############################################
+        
+        output = model(imgs)
+        
+        #select index with maximum prediction score
+        pred = output.max(1, keepdim=True)[1]
+        correct += pred.eq(labels.view_as(pred)).sum().item()
+        total += imgs.shape[0]
+    return correct / total
+
+def train(model, train_data, val_data, learning_rate=0.001, batch_size=64, num_epochs=1):
+    train_loader = torch.utils.data.DataLoader(data, batch_size=batch_size)
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(model.parameters(), lr=learning_rate, momentum=0.9)
+
+    iters, losses, train_acc, val_acc = [], [], [], []
+
+    # training
+    n = 0 # the number of iterations
+    for epoch in range(num_epochs):
+        for imgs, labels in iter(train_loader):
+
+            #############################################
+            # To Enable GPU Usage
+            if torch.cuda.is_available():
+              imgs = imgs.cuda()
+              labels = labels.cuda()
+            #############################################
+              
+            out = model(imgs)             # forward pass
+
+            loss = criterion(out, labels) # compute the total loss
+            loss.backward()               # backward pass (compute parameter updates)
+            optimizer.step()              # make the updates for each parameter
+            optimizer.zero_grad()         # a clean up step for PyTorch
+
+            # save the current training information
+            iters.append(n)
+            losses.append(float(loss)/batch_size)             # compute *average* loss
+            train_acc.append(get_accuracy(model, train_data)) # compute training accuracy 
+            val_acc.append(get_accuracy(model, val_data))  # compute validation accuracy
+            n += 1
+
+    # plotting
+    plt.title("Training Curve")
+    plt.plot(iters, losses, label="Train")
+    plt.xlabel("Iterations")
+    plt.ylabel("Loss")
+    plt.show()
+
+    plt.title("Training Curve")
+    plt.plot(iters, train_acc, label="Train")
+    plt.plot(iters, val_acc, label="Validation")
+    plt.xlabel("Iterations")
+    plt.ylabel("Training Accuracy")
+    plt.legend(loc='best')
+    plt.show()
+
+    print("Final Training Accuracy: {}".format(train_acc[-1]))
+    print("Final Validation Accuracy: {}".format(val_acc[-1]))
+
+baseline_model = Baseline()
+#TRAIN BASELINE ...
+baseline_model.cuda() #USE GPU!
+train(baseline_model, train_data, val_data, 0.001, 64, 5)
