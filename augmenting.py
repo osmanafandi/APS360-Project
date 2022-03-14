8d1475035ee9b92a485bcc752770d0860347c2b5
Osman Afandiyev <osman.afandiyev@mail.utoronto.ca>
2022-03-13 21:57:33 -0400

    script for augmenting the images: Certain directory structure needs to be followed

---
+++ augmenting.py
@@ -0,0 +1,44 @@
+import numpy as np
+import time
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import torchvision
+from torch.utils.data.sampler import SubsetRandomSampler
+import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
+import math
+import PIL
+from torchvision.utils import save_image
+import os
+
+
+dirs = os.listdir("./Final Handpicked")
+
+for path in dirs:
+    data_path = "./Final Handpicked/{}".format(path)
+    transform = transforms.Compose([transforms.Resize((224, 224)),
+                                    transforms.ToTensor()])
+
+
+    original_dataset = torchvision.datasets.ImageFolder(data_path, transform=transform)
+
+    dataset = []
+    for i in range(len(original_dataset)):
+        
+        # Transofrmations
+        dataset.append(transforms.RandomHorizontalFlip(p=1)(original_dataset[i][0])) # Horizontal Flip
+        dataset.append(transforms.RandomPerspective(distortion_scale=0.5, p=1)(original_dataset[i][0])) # Turns in 3d
+        dataset.append(transforms.RandomRotation(degrees=(-45,45))(original_dataset[i][0])) # Rotates within given range
+        dataset.append(transforms.RandomInvert(p=1)(original_dataset[i][0])) # Changes color
+
+        # Original image copy
+        dataset.append(original_dataset[i][0])
+
+
+    k = 0
+    for i in range(len(dataset)):
+                save_image(dataset[i], "./DatasetAugmented/{}Augmented/{}.jpg".format(path,k))
+        k += 1
+
